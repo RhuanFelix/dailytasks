7b17e6b6b253da5ac392e5bc8e450a59e07d73dc
RhuanFelix <rhuansantosfelix@gmail.com>
2025-09-10 14:38:37 -0300

    feat: adição do método abstrato findByTitulo na interface CategoriaRepository

--- src/main/java/com/todolist/dailytasks/repository/CategoriaRepository.java
+++ src/main/java/com/todolist/dailytasks/repository/CategoriaRepository.java
@@ -3,5 +3,8 @@ package com.todolist.dailytasks.repository;
 import com.todolist.dailytasks.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    Optional<Categoria> findByTitulo(String titulo);
 }
